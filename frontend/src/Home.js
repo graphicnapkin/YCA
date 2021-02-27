edcc52ef3f0bf17b9b9532e8dd8ea668128be580
Jonathan Carter <graphicnapkin@me.com>
2021-02-26 19:39:28 -0500

    import bootstrap, install bootstrap package, and import some bootStrap componenets

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Jumbotron } from 'react-bootstrap'
 
 const Home = () => {
     return (
-        <h1 >
-            This is the home page
-        </h1>
+        <Jumbotron >
+            <h1 style={{textAlign: 'center'}}>YouTube Content Analyser</h1>
+        </Jumbotron>
     )
 }
 
