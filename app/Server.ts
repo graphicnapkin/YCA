dd4d52b455cdff00c79e9e691885a9bed86d805a
Andrew J Carter <andrew.jeremiah.carter@gmail.com>
2021-02-27 02:11:48 -0700

    removes some no longer applicable comments

--- app/Server.ts
+++ app/Server.ts
@@ -29,10 +29,7 @@ export class Server {
     this.app.get(
       '/api/search',
       async (req: Request, res: Response): Promise<any> => {
-        // This would be a good function to make async in the future, for now let's stick to sync...
-        //  Get the api request in the correct searchString format
         const searchString = req.query.q
-        // This is maxResults per page of response
         const maxResults = 100
 
         // Single page form of API request:
@@ -40,10 +37,7 @@ export class Server {
         // Next-page-token form of API request:
         // https://www.googleapis.com/youtube/v3/commentThreads?key={your_api_key}&textFormat=plainText&part=snippet&videoId={video_id}&maxResults=100&pageToken={nextPageToken}
 
-        // This try block should only include the fetch call.
-        //  Try blocks should be ideally one operation, not *~4~*
         try {
-          //jankey string concatenation so that we avoid 6000 characters
           const response = await fetch(
             'https://www.googleapis.com/youtube/v3/commentThreads' +
               `?key=${process.env.REACT_APP_YOUTUBE_API_KEY}` +
