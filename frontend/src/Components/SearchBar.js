598d17a9ada74d79dd3b9bc57f227cb47ea55168
Jonathan Carter <graphicnapkin@me.com>
2021-02-27 15:42:07 -0500

    implimented fetch with dirty, drirty results

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,29 +1,47 @@
 import React, { useState } from 'react'
-import {Form, Button} from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
+import { getYoutubeVideoIdFromURL, getComments } from '../util/globals.ts'
 
 const SearchBar = () => {
-
   const [videoURL, setVideoURL] = useState('')
+  const [results, setResults] = useState()
 
-  function handleSubmit (event) {
+  async function handleSubmit(event) {
     event.preventDefault()
-    console.log("state of videoURL at submit: ", videoURL);
+    let id = ''
+    try {
+      id = getYoutubeVideoIdFromURL(videoURL)
+      const res = await getComments(id)
+      setResults(res)
+      // setResults(JSON.parse(res, null, 2))
+    } catch (error) {
+      ////should build notification banner componet to display messages
+    }
+    //console.log('state of videoURL at submit: ', videoURL)
   }
-
   return (
-    <div class='grid-container'>
+    <div className='grid-container'>
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="videoURLSubmit">
+        <Form.Group controlId='videoURLSubmit'>
           <Form.Label>Link to video:</Form.Label>
-          <Form.Control type="text"
-            value={ videoURL }
-            placeholder="Enter a youtube video URL..."
-            onChange={(e) => { setVideoURL(e.target.value) }} />
+          <Form.Control
+            type='text'
+            value={videoURL}
+            placeholder='Enter a youtube video URL...'
+            onChange={(e) => {
+              setVideoURL(e.target.value)
+            }}
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant='primary' type='submit'>
           Submit
         </Button>
       </Form>
+      {results && (
+        <div style={{ fontSize: 8 }}>
+          {JSON.stringify(results.results, null, 4)}
+        </div>
+      )}
     </div>
   )
 }
---
+++ frontend/src/util/globals.ts
@@ -0,0 +1,33 @@
+import { CommentThreadsResult } from './commonInterfaces'
+
+export function getYoutubeVideoIdFromURL(url: string): string {
+  const validURL = URLParse(url)
+  if (!validURL.isValid) throw TypeError
+  return validURL.id || ''
+}
+
+export async function getComments(id: string) {
+  //will need to refactor to handle paging of results
+  console.log('starting search')
+  const response = await fetch(`http://localhost:8080/api/search?q=${id}`)
+  const results: CommentThreadsResult = await response.json()
+  return results
+}
+
+function URLParse(url: string): { isValid: boolean; id?: string } {
+  //  The captured groups are:
+  //protocol
+  //subdomain
+  //domain
+  //path
+  //video code
+  //query string
+  const pattern = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+  if (!pattern.test(url)) {
+    return { isValid: false }
+  }
+  const matches = pattern.exec(url)
+  if (!matches?.[5]) return { isValid: false }
+
+  return { isValid: true, id: matches[5] }
+}
