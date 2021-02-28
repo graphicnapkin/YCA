559199ed83c76a8e500ae14db98bec9cd6e219e1
Jon Carter <graphicnapkin@me.com>
2021-02-28 14:14:14 -0500

    styled home

--- frontend/src/Components/SearchBar.tsx
+++ frontend/src/Components/SearchBar.tsx
@@ -1,52 +1,52 @@
-import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom'
-import { getYoutubeVideoIdFromURL, getComments } from '../util/globals'
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { getYoutubeVideoIdFromURL, getComments } from '../util/globals';
 
 interface SearchBarProps {
   resultSetHook: Function;
 }
 
-export const SearchBar: React.FC<SearchBarProps> = ({ resultSetHook }) => {
-  const [videoURL, setVideoURL] = useState('')
-  const history = useHistory()
+export const SearchBar: React.FC<SearchBarProps> = ( { resultSetHook } ) => {
+  const [ videoURL, setVideoURL ] = useState( '' );
+  const history = useHistory();
 
-  async function handleSubmit(event: any) {
-    event.preventDefault()
-    let id = ''
+  async function handleSubmit( event: any ) {
+    event.preventDefault();
+    let id = '';
     try {
-      id = getYoutubeVideoIdFromURL(videoURL)
-      const res = await getComments(id)
-      resultSetHook(res)
-      history.push('./results')
-    } catch (error) {
-      console.log(error)
+      id = getYoutubeVideoIdFromURL( videoURL );
+      const res = await getComments( id );
+      resultSetHook( res );
+      history.push( './results' );
+    } catch ( error ) {
+      console.log( error );
       ////should build notification banner componet to display messages
     }
   }
   return (
     <div className='grid-container'>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={ handleSubmit }>
         <Form.Group controlId='videoURLSubmit'>
           <Form.Label>Link to video:</Form.Label>
           <Form.Control
             type='text'
-            value={videoURL}
+            value={ videoURL }
             placeholder='Enter a youtube video URL...'
-            onChange={(e) => {
-              setVideoURL(e.target.value)
-            }}
+            onChange={ ( e ) => {
+              setVideoURL( e.target.value );
+            } }
           />
         </Form.Group>
         <Button variant='primary' type='submit'>
           Submit
         </Button>
       </Form>
-       {/*results && (
+      {/*results && (
         <div style={{ fontSize: 8 }}>
           {JSON.stringify(results.results, null, 4)}
         </div>
        )*/}
     </div>
-  )
-}
+  );
+};
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import { Jumbotron } from 'react-bootstrap'
-import { SearchBar } from '../Components/SearchBar'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import * as CSS from 'csstype';
+import { Jumbotron } from 'react-bootstrap';
+import { SearchBar } from '../Components/SearchBar';
+import { Container } from 'react-bootstrap';
 
 interface HomeProps {
   resultSetHook: Function;
 }
 
-export const Home: React.FC<HomeProps> = (props) => {
+export const Home: React.FC<HomeProps> = ( props ) => {
   return (
-    <Container style={containerStyle}>
-      <Jumbotron style={jumboStyle}>
-        <div>YouTube Comment Analyser</div>
+    <Container style={ containerStyle }>
+      <Jumbotron style={ jumboStyle }>
+        <div style={ headerStyle }>YouTube Comment Analyser</div>
       </Jumbotron>
-      <SearchBar resultSetHook={props.resultSetHook} />
+      <SearchBar resultSetHook={ props.resultSetHook } />
     </Container>
-  )
-}
+  );
+};
 
 // HeaderStyle not TS compatible?
-const headerStyle = {
+const headerStyle: CSS.Properties = {
   fontSize: '2.5rem',
   fontWeight: 'bold',
   textAlign: 'center'
-}
+};
 
-const jumboStyle = {
-  borderRadius: 120
-}
+const jumboStyle: CSS.Properties = {
+  borderRadius: '120%'
+};
 
-const containerStyle = {
-  paddingTop: 225,
-  paddingBottom: 8,
+const containerStyle: CSS.Properties = {
+  paddingTop: '225px',
+  paddingBottom: '8px',
   fontSize: '2rem'
-}
+};
